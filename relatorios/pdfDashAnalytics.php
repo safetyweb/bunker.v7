405cc85edff810c456d378e742ab93fb5b465ebb
LucasDevelopers <66512691+LucasDevelopers@users.noreply.github.com>
2025-07-04 15:32:17 -0300

    ahju

--- relatorios/pdfDashAnalytics.php
+++ relatorios/pdfDashAnalytics.php
@@ -1,5 +1,12 @@
 <?php
 
+if ($_SESSION['SYS_COD_EMPRESA'] == 2) {
+	echo fnDebug('true');
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
+
 $dt_exibe = "";
 $qtd_clientes = 0;
 $qtd_cli_novos = 0;
